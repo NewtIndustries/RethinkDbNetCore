2b31f64811c3ce2be7f91a89b82b52a451e856b5
newtindustries <info@newtindustries.com>
2018-04-27 14:24:16 -0500

    Start of server stuff

--- src/RethinkDbNetCore/RethinkServer.cs
+++ src/RethinkDbNetCore/RethinkServer.cs
@@ -1,15 +1,20 @@
 ﻿using RethinkDb.Driver.Net;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using RethinkDb.Driver;
 
 namespace RethinkDbNetCore
 {
+    // https://www.c-sharpcorner.com/article/building-a-tcp-server-in-net-core-on-ubuntu/
     public class RethinkServer
     {
         private Connection _connection;
-
+        private TcpListener _tcpListener;
+        private bool _active;
         public RethinkServer()
         {
             _connection = RethinkDB.R.Connection()
@@ -17,10 +22,32 @@ namespace RethinkDbNetCore
                 .Port(RethinkDBConstants.DefaultPort)
                 .Timeout(60)
                 .Connect();
-	    var obj = new { X = 0, Y = 1, Z = 2 };
-	    // var result = RethinkDB.R.Db("test").TableCreate("vectors").Run(_connection);
-	    var result = RethinkDB.R.Db("test").Table("vectors").Insert(obj).Run(_connection);
-	    Console.WriteLine(result.ToString());
+            IPAddress address = IPAddress.Parse("127.0.0.1");
+            _tcpListener = new TcpListener(address, 4242);
+            _tcpListener.Start();
+            _active = true;
+            Listen();
+            //var obj = new { X = 0, Y = 1, Z = 2 };
+            //// var result = RethinkDB.R.Db("test").TableCreate("vectors").Run(_connection);
+            //var result = RethinkDB.R.Db("test").Table("vectors").Insert(obj).Run(_connection);
+            //Console.WriteLine(result.ToString());
+        }
+
+        private void Listen()
+        {
+            if (_tcpListener != null && _active)
+            {
+                while (true)
+                {
+                    Console.WriteLine("Waiting for client...");
+                    var clientTask = _tcpListener.AcceptTcpClientAsync();
+                    if (clientTask.Result != null)
+                    {
+                        Console.WriteLine("Client connected.  Waiting for data");
+
+                    }
+                }
+            }
         }
     }
 }
